0520595b51e6720e421cda728eb6d2a3f290f25f
Tony Yang <superzoro168@gmail.com>
2022-07-02 12:16:15 +0800

    Configure layout

--- app/src/main/java/com/example/tonycomposedojo/MainActivity.kt
+++ app/src/main/java/com/example/tonycomposedojo/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,13 +32,18 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(message: Message) {
-    Row {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.profile),
-            contentDescription = "Content profile picture"
+            contentDescription = "Content profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
         )
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
             Text(text = message.author)
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = message.body)
         }
     }
