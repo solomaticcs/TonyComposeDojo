1ef9117d37ca7e28db7fd9f0073fd75faed573bb
Tony Yang <superzoro168@gmail.com>
2022-07-02 12:10:58 +0800

    Using Column and add an image element

--- app/src/main/java/com/example/tonycomposedojo/MainActivity.kt
+++ app/src/main/java/com/example/tonycomposedojo/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.tonycomposedojo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -25,8 +29,16 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(message: Message) {
-    Text(text = message.author)
-    Text(text = message.body)
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "Content profile picture"
+        )
+        Column {
+            Text(text = message.author)
+            Text(text = message.body)
+        }
+    }
 }
 
 @Preview
