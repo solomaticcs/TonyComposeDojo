1c1adc2ed86c829efba5db9fbec020500193e1fc
Tony Yang <superzoro168@gmail.com>
2022-07-02 11:06:15 +0800

    Lesson 1: Composable functions

--- app/src/main/java/com/example/tonycomposedojo/MainActivity.kt
+++ app/src/main/java/com/example/tonycomposedojo/MainActivity.kt
@@ -3,41 +3,26 @@ package com.example.tonycomposedojo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.tonycomposedojo.ui.theme.TonyComposeDojoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TonyComposeDojoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(name = "Android")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun MessageCard(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    TonyComposeDojoTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard(name = "Android")
 }
